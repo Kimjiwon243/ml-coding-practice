7807ddae59587334bdf0fc50cbf891f83d36fbd5
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-18 17:00:20 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -283,4 +283,9 @@ for i in test_list:
 # for 반복문2
 number = 0
 for score in [90, 25, 67, 45, 93]:
-    
+    number += 1
+
+    if score > 60:
+        print("%d번 학생은 합격입니다." % number)
+    else:
+        print("%d번 학생은 불합격입니다.")
