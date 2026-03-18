99227c745e901cf6d703eb9fb25e2ced27d3376b
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-18 18:15:51 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -47,4 +47,6 @@ print(netflix.head())
 print(netflix['release_year'])
 print(netflix['release_year'] > 2015)
 
-more2015 = netflix
+more2015 = netflix[netflix['release_year'] > 2015]
+print(more2015.head(10))
+
