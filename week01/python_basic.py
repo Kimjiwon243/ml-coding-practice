1591ae757dfc178527983630e061781dfc9bfda0
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-18 16:16:30 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -1,15 +1,15 @@
 a = 3
 b = 4
 
-#지수승
+# 지수승
 print(a ** b)
 print(a ** 3)
 
-#나머지 연산
+# 나머지 연산
 print(a % b)
 print(7 % 3)
 
-#나눗셈 몫 구하기
+# 나눗셈 몫 구하기
 print(a // b)
 print(7 // 3)
 
@@ -21,3 +21,18 @@ Python'''
 print(s3)
 
 head = "Python"
+tail = " is fun"
+print(head + tail)
+
+# 문자열 곱하기
+print(head * 2)
+print("=" * 5)
+
+# 문자열 인덱싱
+a = "Now is better than never"
+print(a[0])
+print(a[4])
+print(a[-1])
+print(a[-2])
+
+#문
