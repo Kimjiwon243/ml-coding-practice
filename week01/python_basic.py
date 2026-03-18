b4d58a3914165707e6980c280f8d6b30f364186b
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-18 17:02:20 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -296,3 +296,14 @@ while i < 5:
     i += 1
     print('*' * i)
 
+# 함수 정의
+def sum1(a, b):
+    x = a + b
+    return x
+
+def sum2(*args):
+    x = 0
+    for i in args:
+        x += i
+    return x
+
