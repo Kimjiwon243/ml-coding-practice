a1ef69b389077dddb28281e2fa6ced8d703abb97
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-18 18:26:22 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -90,3 +90,8 @@ print(sample_df.reset_index(drop=True))          # 기존 인덱스는 제거하
 
 print(sample_df.reset_index('var_1'))
 
+print(sample_df.describe())
+print(sample_df.std())
+print(sample_df.agg(['count', 'mean', 'std', 'min', 'max']))
+
+file_url 
