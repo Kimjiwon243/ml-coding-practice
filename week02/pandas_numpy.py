8c4f03f04cfd7f49c8399c4f902696f07ee2c09d
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-18 18:30:52 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -116,3 +116,11 @@ data = {
     'salary': [70000.00, 80000.00, 90000.00, 60000.00, 95000.00]
 }
 
+# Dataframe 생성
+df = pd.DataFrame(data)
+print(df.head())
+
+#개인별 과목 점수의 평균값 계산 (axis=1)
+df['average'] = df[['math', 'science', 'english']].mean(axis=1)
+print(df)
+
