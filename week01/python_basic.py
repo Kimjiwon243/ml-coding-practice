cf88e4084e5f6d17e18987c7399c0e0ace77b3b7
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-18 16:37:50 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -141,4 +141,22 @@ print(a.index(9))
 a.insert(0, 99)
 print(a)
 
-# 원소 삭
+# 원소 삭제
+a.remove(99)
+print(a)
+
+b = [1, 2, 3]
+print(b.pop())
+print(b)
+
+# 특정 원소값의 개수
+a = [2, 1, 0, 2, 3, 2, 4, 2]
+print(a.count(2))
+
+# 튜플 만들기
+t1 = (1, )
+t2 = (1, 2, 3)
+t3 = 1, 2, 3
+t4 = (1, 2, (3, 4), ('Life', 'is'))
+
+# 튜플 인덱싱
