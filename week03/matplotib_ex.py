f46b4e5631674683af5a8efc1588bec4b50e1d74
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-25 19:15:28 +0900

    Auto commit: 2026-03-25 19:15 - 저장됨

--- week03/matplotib_ex.py
+++ week03/matplotib_ex.py
@@ -67,3 +67,7 @@ bars = plt.barh(survived_count.index, survived_count, color=['darkturquoise', 's
 plt.title('Survived Count by Gender on Titanic')
 plt.xlabel('Count')
 plt.ylabel('Gender')
+plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='upper right')
+
+# 차이 강조를 위해 수평선 추가
+plt.axvline(x=survived_counts['male'], color='gray', linestyle='--', linewidth=1)
