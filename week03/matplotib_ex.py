f53b3d2081cbefc46d558436fb03c2edcc48be1b
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-25 19:11:09 +0900

    Auto commit: 2026-03-25 19:11 - 저장됨

--- week03/matplotib_ex.py
+++ week03/matplotib_ex.py
@@ -46,4 +46,11 @@ plt.title('Survived Count by Embarked Port on Titanic')
 plt.xlabel('Embarked Port')
 plt.ylabel('Count')
 plt.xticks(survived_count.index, ['Southampton', 'Cherbourg', 'Queenstown'])
-plt.legend(['Survived'], )
+plt.legend(['Survived'], loc='upper right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# 생존자 수 표시
+for i, value in enumerate(survived_count):
+    plt.text(i, value + 1, str(value), ha='center', va='bottom')
+
+    
