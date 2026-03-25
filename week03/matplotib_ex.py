83f5668196fcbbb0bcdc6253ebdd988e3e991a93
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-25 19:39:24 +0900

    Auto commit: 2026-03-25 19:39 - 저장됨

--- week03/matplotib_ex.py
+++ week03/matplotib_ex.py
@@ -108,4 +108,4 @@ print(survived_count)
 # 파이 차트 그리기 
 plt.figure(figsize=(8, 8))
 plt.pie(survived_count, labels=['Not Survived', 'Survived'], color=['orange', 'gold'],
-        autopct=)
+        autopct='%0.1f%%', startangle=90, shadow=True, explode=(0, 0.1))
