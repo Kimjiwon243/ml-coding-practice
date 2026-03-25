9e03755ef91ca0758ebf56233dd247f5ea714f46
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-25 20:31:34 +0900

    Auto commit: 2026-03-25 20:31 - 저장됨

--- week03/matplotib_ex.py
+++ week03/matplotib_ex.py
@@ -2,7 +2,7 @@
 # 타이타닉 데이터셋 불러오기
 import pandas as pd
 
-# 타이타닉 CSV 파일 불러오가
+# 타이타닉 CSV 파일 불러오기
 titanic = pd.read_csv('3.1.1.titanic.csv')
 
 # head( ) 함수를 출력하여 타이타닉 데이터셋의 구성을 간단히 살펴보기
@@ -15,7 +15,7 @@ print(titanic.info())
 
 """### **선 그래프 : 객실 등급에 따른 생존율 표시하기**"""
 
-#객실 등급에 따른 생존자와 사망자의 평균 계산
+# 객실 등급에 따른 생존자와 사망자의 평균 계산
 pclass_survived_mean = titanic.groupby('Pclass')['Survived'].mean().reset_index()
 pclass_survived_mean
 
@@ -25,9 +25,9 @@ import matplotlib.pyplot as plt
 # 선 그래프 그리기
 plt.plot(pclass_survived_mean['Pclass'], pclass_survived_mean['Survived'],
          marker='o', linestyle='-', color='violet')
-plt.title('Survival Rate Variation Across Passenger Calsses')
+plt.title('Survival Rate Variation Across Passenger Classes')
 plt.xlabel('Pclass')
-plt.ylabel('Surviva; Rate')
+plt.ylabel('Survival Rate')
 plt.xticks([1, 2, 3])
 plt.grid(True)
 plt.savefig('Figure01.png')         # 결과를 그림파일로 저장
@@ -36,22 +36,22 @@ plt.close()                         # 다음 Plot을 새로 그리기 위해 plt
 """### **수직 막대 그래프 : 각 승선 항구에 따른 생존자 수 확인하기**"""
 
 # 승선 항구에 따른 생존자의 수 계산
-survived_count = titanic[titanic['Survived'] == 1]['Embarked'].value_counts()
-print(survived_count)
+survived_counts = titanic[titanic['Survived'] == 1]['Embarked'].value_counts()
+print(survived_counts)
 
 # 막대 그래프 그리기
-plt.bar(survived_count.index, survived_count,
-        color = ['mediumorchid', 'darkvioet', 'indigo'])
-plt.title('Survived Count by Embarked Port on Titanic')
+plt.bar(survived_counts.index, survived_counts,
+        color = ['mediumorchid', 'darkviolet', 'indigo'])
+plt.title('Survived Counts by Embarked Port on Titanic')
 plt.xlabel('Embarked Port')
 plt.ylabel('Count')
-plt.xticks(survived_count.index, ['Southampton', 'Cherbourg', 'Queenstown'])
+plt.xticks(survived_counts.index, ['Southampton', 'Cherbourg', 'Queenstown'])
 plt.legend(['Survived'], loc='upper right')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 # 생존자 수 표시
-for i, value in enumerate(survived_count):
-    plt.text(i, value + 1, str(value), ha='center', va='bottom')
+for i, value in enumerate(survived_counts):
+  plt.text(i, value + 1, str(value), ha='center', va='bottom')
 
 plt.savefig('Figure02.png')
 plt.close()
@@ -59,12 +59,12 @@ plt.close()
 """### **수평 막대 그래프 : 성별에 따른 생존자 수 확인하기**"""
 
 # 성별에 따른 생존자의 수 계산
-survived_count = titanic[titanic['Survived'] == 1]['Sex'].value_count()
-print(survived_count)
+survived_counts = titanic[titanic['Survived'] == 1]['Sex'].value_counts()
+print(survived_counts)
 
 # 수평 막대 그래프 그리기
-bars = plt.barh(survived_count.index, survived_count, color=['darkturquoise', 'salmon'])
-plt.title('Survived Count by Gender on Titanic')
+bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise', 'salmon'])
+plt.title('Survived Counts by Gender on Titanic')
 plt.xlabel('Count')
 plt.ylabel('Gender')
 plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='upper right')
@@ -73,17 +73,17 @@ plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='upper right')
 plt.axvline(x=survived_counts['male'], color='gray', linestyle='--', linewidth=1)
 
 # 생존자 수 표시
-for i, value in enumerate(survived_count):
-    plt.text(value + 1, i, str(value), ha='left', va='center')
+for i, value in enumerate(survived_counts):
+  plt.text(value + 1, i, str(value), ha='left', va='center')
 
-    plt.savefig('Figure03.png')
-    plt.close()
+plt.savefig('Figure03.png')
+plt.close()
 
 """### **산점도 그래프 : 나이와 요금, 생존 여부 확인하기**"""
 
 print(titanic.info(), '\n')
 
-# 결측지 처리 
+# 결측치 처리
 titanic = titanic.dropna(subset=['Age', 'Fare', 'Survived'])
 print(titanic.info())
 
@@ -91,7 +91,7 @@ print(titanic.info())
 plt.figure(figsize=(12, 8))
 scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic['Survived'],cmap='Set2', alpha=0.7)
 
-plt.title('Age and Fare Relationship with Survival on Titanic')
+plt.title('Age and Fare Relationship with Survival on the Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
 plt.legend(handles=scatter.legend_elements()[0], title='Survived',
@@ -101,16 +101,16 @@ plt.close()
 
 """### **파이 차트 : 생존자, 사망자 비율 표현하기**"""
 
-# 사망자와 생존자와 수 계산
-survived_count = titanic['Survived'].value_counts()
-print(survived_count)
+# 사망자와 생존자의 수 계산
+survived_counts = titanic['Survived'].value_counts()
+print(survived_counts)
 
-# 파이 차트 그리기 
+# 파이 차트 그리기
 plt.figure(figsize=(8, 8))
-plt.pie(survived_count, labels=['Not Survived', 'Survived'], color=['orange', 'gold'],
+plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange', 'gold'],
         autopct='%0.1f%%', startangle=90, shadow=True, explode=(0, 0.1))
 
-plt.title('Survival Distributon on the Titanic')
+plt.title('Survival Distribution on the Titanic')
 plt.savefig('Figure05.png')
 plt.close()
 
@@ -119,7 +119,7 @@ plt.close()
 # 처리 전
 print(titanic.info(), '\n')
 
-# 나이 결측지 처리 후 
+# 나이 결측치 처리 후
 titanic = titanic.dropna(subset=['Age'])
 print(titanic.info())
 
@@ -136,7 +136,7 @@ plt.close()
 
 ## 히트맵 : 두 변수의 상관 관계를 표시하기**
 
-# 결측지 처리
+# 결측치 처리
 titanic = titanic.dropna(subset=['Age', 'Fare'])
 
 # 상관 행렬 계산
@@ -189,7 +189,7 @@ plt.close()
 
 ## **박스 플롯 : 승객 나이의 데이터 분포, 중앙값, 이상치 살펴보기**
 
-# 결측지 처리
+# 결측치 처리
 titanic = titanic.dropna(subset=['Age'])
 print(titanic.info())
 
