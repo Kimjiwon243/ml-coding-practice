1c39734b6ecb3a52ae5ab24bed109d1066891383
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-25 19:37:11 +0900

    Auto commit: 2026-03-25 19:37 - 저장됨

--- week03/matplotib_ex.py
+++ week03/matplotib_ex.py
@@ -100,3 +100,9 @@ plt.savefig('Figure04.png')
 plt.close()
 
 """### **파이 차트 : 생존자, 사망자 비율 표현하기**"""
+
+# 사망자와 생존자와 수 계산
+survived_count = titanic['Survived'].value_counts()
+print(survived_count)
+
+# 파이 차트 그리기 
