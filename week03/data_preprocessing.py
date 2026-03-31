69e3ac198ba21d80ca65496f30a9fe07c19f2a64
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-03-31 17:27:32 +0900

    Auto commit: 2026-03-31 17:27 - 저장됨

--- week03/data_preprocessing.py
+++ week03/data_preprocessing.py
@@ -43,4 +43,16 @@ housing_labels = strat_train_set["median_house_value"].copy()
 
 # 데이터 정제
 # null 값이 있는 행 확인하기
-nu
+null_rows_idx = housing.isnull().any(axis=1)
+housing.loc[null_rows_idx].head()
+
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(strategy="median")
+
+# 수치형 특성만 추출
+housing_num = housing.select_dtypes(include=[np.number])
+housing_num.head()
+
+imputer.fit(housing_num)
+
