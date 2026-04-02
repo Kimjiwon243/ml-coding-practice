f204faf96de4722b21f648a8f6455be49d2ac477
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-04-02 16:24:54 +0900

    Auto commit: 2026-04-02 16:24 - 저장됨

--- week03/data_preprocessing.py
+++ week03/data_preprocessing.py
@@ -45,3 +45,21 @@ imputer.fit(housing_num)
 
 print(imputer.statistics_)           # imputer 결과 값
 print(housing_num.median().values)   # 수동으로 계산한 중간값
+
+# 훈련 세트의 누락값을 imputer가 학습한 값으로 채우기
+X = imputer.transform(housing_num)
+
+imputer.feature_names_in_
+
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing_num.index)
+housing_tr.loc[null_row_idx].head()
+
+# 이상치 삭제
+from sklearn.ensemble import IsolationForest
+
+isolation_forest = IsolationForest(random_state=42)
+outlier_pred = isolation_forest.fit_predict(X)
+
+outlier_pred
+
