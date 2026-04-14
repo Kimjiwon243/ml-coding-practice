b09eb598d798b4c8ffe8e6618dd68ab8dd97e29f
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-04-14 16:17:04 +0900

    Auto commit: 2026-04-14 16:17 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -63,8 +63,8 @@ def getRequestUrl(url):
             return response.read().decode('utf-8')
     except Exception as e:
         print(e)
-        print("[%s] Error for URL : %s" % (datetime.datetime.now()))
-        return response.read().decode('utf-8')
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
     
     
 
