59b095b563e0378e681540217095b7ea1d31222f
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-04-14 16:46:50 +0900

    Auto commit: 2026-04-14 16:46 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -46,4 +46,5 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 
            natName = jsonData['response']['body']['item']['item']['natKorNm']
            natName = natName.replace(' ', '')
-           num = ['response']['body']['items']['item']['num']
+           num = jsonData['response']['body']['items']['item']['num']
+           ed = jsonData
