9c7d05ac94a53187a0f91e2ba853b27c7f5055f7
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-04-14 16:35:16 +0900

    Auto commit: 2026-04-14 16:35 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -31,4 +31,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
   jsonResult = []
   result = []
 
-  
+  for year in range(nStartYear, nEndYear+1):
+     for month in range(1, 13):
+        yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+        jsonData = getTourismStatsltem(yyyymm, nat_cd, ed_cd)
