e360fff7b014371ab9d2424040ff89271477c5c1
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-04-14 16:09:26 +0900

    Auto commit: 2026-04-14 16:09 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -37,4 +37,8 @@ def main():
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
 def getNaverSearch(node, srcText, Page_start, display):
-    base = 
+    base = "https://openapi.naver.com/v1/search"
+node = "/%s.json" % node
+parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+
+
