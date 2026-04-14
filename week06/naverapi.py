072a865a77b48a96f55df9d47cf6422db08bce92
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-04-14 16:00:14 +0900

    Auto commit: 2026-04-14 16:00 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -4,4 +4,14 @@ import datetime
 import json
 
 client_id = 'Zb1qCprIQO1X3FYthAOp'
-client_secret = ''
+client_secret = '13pox1GnON'
+
+def main():
+
+    node = 'news'
+    srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse
