18c227e338d2dc89113bcece8b028ed49a929705
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-05 13:18:51 +0900

    Auto commit: 2026-05-05 13:18 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -37,3 +37,6 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 # 이진 분류기 훈련
 y_train_5 = (y_train == '5')  # 5인 경우는 True, 나머지는 False
+y_test_5 = (y_test == '5')
+
+from sklearn.linear_model import SGDClassifier
