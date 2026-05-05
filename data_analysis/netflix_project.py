bea92b07deac8770be6b79f4c1eed23c54dff762
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-05 15:03:04 +0900

    Auto commit: 2026-05-05 15:03 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -185,3 +185,9 @@ cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#
 
 sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidths=2.5, annot=True, fmt='.0%')
 
+plt.suptitle('Target ages proportion of total content by country',
+             fontweight='bold', fontfamily='serif', fontsize=15)
+plt.title('Here we see interesting differences between countries. Most shows in South Korea are targeted to adults, for instance.',
+          fontsize=12, fontfamily='serif')
+plt.show()
+
