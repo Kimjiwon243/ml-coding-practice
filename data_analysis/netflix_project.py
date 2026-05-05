f7c702a7cb47424d6759f7c3abd4c6982b93a494
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-05 14:23:57 +0900

    Auto commit: 2026-05-05 14:23 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -21,4 +21,4 @@ netflix.info()
 
 # 넷플릭스 결측치 비율 확인하기
 for i in netflix.columns : 
-    missing_rate = netflix[i].isnull().sum() / len(netflix) * 100
+    missing_rate = netflix[i].isna().sum() / len(netflix) * 100
