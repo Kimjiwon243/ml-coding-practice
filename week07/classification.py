f032a95b5cf8ed2afa8c7a170f2f3959afa860ec
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-05 13:20:38 +0900

    Auto commit: 2026-05-05 13:20 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -51,4 +51,5 @@ from sklearn.model_selection import cross_val_score
 
 cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring='accuracy')
 
-from sklearn
+from sklearn.dummy import DummyClassifier   
+
