67e35d51617809044b3af6158a29c2944eeb4489
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-10 22:24:40 +0900

    Auto commit: 2026-05-10 22:24 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -2,10 +2,10 @@
 # 선형 SVM 분류
 
 import numpy as np
-import sklearn.datasets import load_iris
-import sklearn.pipeline import make_pipeline
-import sklearn.preprocessing import StandardScaler  
-import sklearn.svm import SVC
+from sklearn.datasets import load_iris
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
 
 iris = load_iris(as_frame=True)
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
