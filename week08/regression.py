ee9ebd46a804bfe2a0ec4d0880323603748c50e8
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-12 17:26:57 +0900

    Auto commit: 2026-05-12 17:26 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 plt.rc('font', size=14)
 plt.rc('axes', labelsize=14, titlesize=14)
-plt.rc(legend, fontsize=14)
+plt.rc('legend', fontsize=14)
 plt.rc(xtick, labelsize=10)
 plt.rc(ytick, labelsize=10)
 
