88db5f435ae9856c155809741a42fecb9982569e
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-30 02:19:28 +0900

    Auto commit: 2026-05-30 02:19 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -76,3 +76,8 @@ plt.axis([1.8, 8.5, 0.55, 0.8])
 plt.grid()
 plt.show()
 
+from sklearn.metrics import silhouette_samples
+from matplotlib.ticker import FixedLocator, FixedFormatter
+
+plt.figure(figsize=(11, 9))
+
