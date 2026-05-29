9d83e2f38423b8e3dc53126e72846dc3acfbeeac
Kimjiwo_243 <andrew1783@sunmoon.ac.kr>
2026-05-30 04:18:19 +0900

    Auto commit: 2026-05-30 04:18 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -119,4 +119,8 @@ plt.show()
 from sklearn.cluster import DBSCAN
 from sklearn.datasets import make_moons
 
-X, 
+X, y =make_moons(n_samples=1000, noise=0.05, random_state=42)
+dbscan = DBSCAN(eps=0.05, min_samples=5)
+dbsen.fit(X)
+
+print(dbscan.labels_[:10])
